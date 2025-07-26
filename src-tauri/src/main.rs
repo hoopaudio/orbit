9bd0820094791a35fc15af3316b1c7575303869d
cuthlehoop <alialabbassi2001@gmail.com>
2025-07-26 22:08:56 +0300

    add system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,41 +3,55 @@
     windows_subsystem = "windows"
 )]
 
-mod command;
+mod commands;
+mod consts;
 mod tray;
 mod window;
 
-use command::{hide, show};
-use tauri::{Listener, Manager};
+use crate::consts::{MAIN_WINDOW_NAME, ORBIT_LABEL, SETTINGS_WINDOW_NAME};
+use crate::tray::Tray;
+use commands::*;
+use std::{
+    str::FromStr,
+    sync::{atomic::AtomicBool, Mutex},
+};
+use tauri::{generate_handler, menu::Menu, Listener, LogicalSize, Manager, Wry};
 use tauri_nspanel::ManagerExt;
 use tauri_plugin_global_shortcut::{Code, Modifiers, Shortcut, ShortcutState};
+use tauri_plugin_log::{Target, TargetKind};
+use tauri_plugin_window_state::{AppHandleExt, StateFlags};
 use window::WebviewWindowExt;
 
-pub const ORBIT_LABEL: &str = "main";
+pub struct Pinned(AtomicBool);
+
+pub struct TrayMenu(Mutex<Menu<Wry>>);
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![show, hide])
-        .plugin(tauri_nspanel::init())
-        .setup(move |app| {
-            // Set activation poicy to Accessory to prevent the app icon from showing on the dock
-            app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+    let flags = StateFlags::POSITION | StateFlags::SIZE;
+    let window_state_plugin = tauri_plugin_window_state::Builder::default().with_state_flags(flags);
+    let log_level = std::env::var("LOG_LEVEL").unwrap_or_else(|_| "info".to_string());
+    let log_level_filter = log::LevelFilter::from_str(&log_level).unwrap_or(log::LevelFilter::Info);
 
-            let handle = app.app_handle();
+    let log_plugin_builder = tauri_plugin_log::Builder::new()
+        .targets([Target::new(TargetKind::LogDir { file_name: None })])
+        .level_for("orbit", log_level_filter)
+        .level_for("reqwest", log_level_filter)
+        .level_for("tokio_tungstenite", log::LevelFilter::Off)
+        .level_for("tungstenite", log::LevelFilter::Off)
+        .level_for("tao", log::LevelFilter::Off);
 
-            let window = handle.get_webview_window(ORBIT_LABEL).unwrap();
-
-            // Convert the window to a spotlight panel
-            let panel = window.to_orbit_panel()?;
-
-            handle.listen(format!("{}_panel_did_resign_key", ORBIT_LABEL), move |_| {
-                // Hide the panel when it's no longer the key window
-                // This ensures the panel doesn't remain visible when it's not actively being used
-                panel.order_out(None);
-            });
-
-            Ok(())
-        })
+    let mut app = tauri::Builder::default()
+        .plugin(window_state_plugin.build())
+        .plugin(tauri_plugin_websocket::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(log_plugin_builder.build())
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+        }))
         // Register a global shortcut (⌘+O) to toggle the visibility of the spotlight panel
         .plugin(
             tauri_plugin_global_shortcut::Builder::new()
@@ -61,7 +75,73 @@ fn main() {
                     }
                 })
                 .build(),
-        )
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        );
+
+    #[cfg(target_os = "macos")]
+    {
+        app = app.plugin(tauri_nspanel::init());
+    }
+
+    app = app
+        .manage(Pinned(AtomicBool::new(false)))
+        .setup(move |app| {
+            // Set activation poicy to Accessory to prevent the app icon from showing on the dock
+            app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+
+            let handle = app.app_handle();
+
+            let window = handle.get_webview_window(ORBIT_LABEL).unwrap();
+
+            #[cfg(not(target_os = "macos"))]
+            window.set_always_on_top(true);
+
+            window.set_decorations(false);
+            window.set_shadow(false);
+
+            // Convert the window to a spotlight panel
+            let panel = window.to_orbit_panel()?;
+
+            handle.listen(format!("{}_panel_did_resign_key", ORBIT_LABEL), move |_| {
+                // Hide the panel when it's no longer the key window
+                // This ensures the panel doesn't remain visible when it's not actively being used
+                panel.order_out(None);
+            });
+
+            Tray::update_tray(app.app_handle());
+
+            Ok(())
+        })
+        .invoke_handler(generate_handler![
+            toggle_pin,
+            get_pin,
+            set_pin,
+            open_devtools,
+            close_settings,
+            open_settings,
+            show,
+            hide
+        ]);
+
+    app.build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|app, event| {
+            if let tauri::RunEvent::WindowEvent {
+                event: tauri::WindowEvent::CloseRequested { api, .. },
+                label,
+                ..
+            } = event
+            {
+                if label == SETTINGS_WINDOW_NAME {
+                    let win = app.get_webview_window(label.as_str()).unwrap();
+                    win.hide().unwrap();
+                }
+
+                if label == MAIN_WINDOW_NAME {
+                    app.save_window_state(StateFlags::POSITION | StateFlags::SIZE);
+                    std::process::exit(0);
+                } else {
+                    api.prevent_close();
+                }
+            }
+        });
 }
